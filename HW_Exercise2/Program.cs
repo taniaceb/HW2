1e1326e33d11c05337d37d32a8063046a4373229
taniaceb <tatjana.cebatul@gmail.com>
2019-07-13 16:12:24 +0300

    Check is magic

--- HW_Exercise2/Program.cs
+++ HW_Exercise2/Program.cs
@@ -6,35 +6,64 @@ namespace HW_Exercise2
     {
         static void Main(string[] args)
         {
-            int[] array, array2;
-            array = NumberChangeToArray(745931);
-            array2 = NumberChangeToArray(593176);
-            Console.WriteLine("Ar du array sutama:" + CheckTwoArrayElement(array, array2)); 
+           int[] array, array2, array3, array4, array5, array6;
+            int j, k, l, m, n;
+            // array = NumberChangeToArray(745931);
+            //  array2 = NumberChangeToArray(593176);
+            //  Console.WriteLine("Ar du array sutama:" + CheckTwoArrayElement(array, array2)); 
 
-            Console.WriteLine(CheckIsNumberDifferent(array));
+            //  Console.WriteLine(CheckIsNumberDifferent(array));
             //SortArrayElement(array);
 
 
-            foreach (int item in array)
-            {
-                Console.Write(item + " ");
-            }
+            //  foreach (int item in array)
+            //  {
+            //     Console.Write(item + " ");
+            //   }
+            
 
             for (int i = 100000; i < 999999; i++)
             {
-                if ( ((i *= 2) < 999999) && ((i *= 3) < 999999) && ((i *= 4) < 999999) && ((i *= 5) < 999999) && ((i *= 6) < 999999))
-                {
-                    NumberChangeToArray(i);
+                j = i * 2;
+                k = i * 3;
+                l = i * 4;
+                m = i * 5;
+                n = i * 6;
+                
 
+                
+                if ( (j < 999999) && (k < 999999) && (l < 999999) && (m < 999999) && (n < 999999))
+                {
+                    
+                    array = NumberChangeToArray(i);
+                    if (CheckIsNumberDifferent(array) == true)
+                    {
+                        
+                        array2 = NumberChangeToArray(j);
+                        
+                        array3 = NumberChangeToArray(k);
+                        
+                        array4 = NumberChangeToArray(l);
+                        
+                        array5 = NumberChangeToArray(m);
+                        
+                        array6 = NumberChangeToArray(n);
+
+                        if (CheckTwoArrayElement(array, array2) ==true && CheckTwoArrayElement(array, array3)==true && CheckTwoArrayElement(array, array4)== true&& CheckTwoArrayElement(array, array5)==true && CheckTwoArrayElement(array, array6)==true)
+                        {
+                            Console.WriteLine("MAGIC NUMBER IS:  " + i );
+                            Console.WriteLine("*2:  " + j);
+                            Console.WriteLine("*3:  " + k);
+                            Console.WriteLine("*4:  " + l);
+                            Console.WriteLine("*5:  " + m);
+                            Console.WriteLine("*6:  " + n);
+                        }
+                    }
                 }
 
             }
 
         }
-
-        
-
-
 
         static int[] NumberChangeToArray(int number)
         {
